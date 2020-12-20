0dfa079673b6e305aa00da695d68c6ce0d624980
enzious <enzi@braindead.io>
2020-12-20 07:37:53 -0600

    chore: package setup

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import {
+  WEB_KEYCODES_TO_KEY_ENUM,
+  DARWIN_KEYCODES_TO_KEY_ENUM,
+  LINUX_KEYCODES_TO_KEY_ENUM,
+  WINDOWS_KEYCODES_TO_KEY_ENUM,
+  MOUSE_BUTTONS_TO_KEY_ENUM,
+} from 'enums';
+
+export {
+  WEB_KEYCODES_TO_KEY_ENUM,
+  DARWIN_KEYCODES_TO_KEY_ENUM,
+  LINUX_KEYCODES_TO_KEY_ENUM,
+  WINDOWS_KEYCODES_TO_KEY_ENUM,
+  MOUSE_BUTTONS_TO_KEY_ENUM,
+};
+
+export const keyCodeToKeyEnum = (platform: string, keyCode: number) => {
+  switch (platform) {
+    case 'web':
+      return WEB_KEYCODES_TO_KEY_ENUM[keyCode];
+    case 'darwin':
+      return DARWIN_KEYCODES_TO_KEY_ENUM[keyCode];
+    case 'linux':
+      return LINUX_KEYCODES_TO_KEY_ENUM[keyCode];
+    case 'win32':
+    case 'win64':
+      return WINDOWS_KEYCODES_TO_KEY_ENUM[keyCode];
+    default:
+      return;
+  }
+};
+
+export const mouseButtonToKeyEnum = (platform: string, mouseButton: number) => {
+  switch (platform) {
+    case 'web':
+    case 'darwin':
+    case 'linux':
+    case 'win32':
+    case 'win64':
+    default:
+      return MOUSE_BUTTONS_TO_KEY_ENUM[mouseButton];
+  }
+};
